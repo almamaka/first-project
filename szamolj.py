996831d4a77a46350109f9e575a9ed1ec84d71d5
almamaka <esztzi@gmail.com>
2019-11-06 19:15:50 +0100

    Számolj weboldal számolás +assert

--- szamolj.py
+++ szamolj.py
@@ -4,8 +4,14 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 
 driver.get("https://almamaka.github.io/first-project/")
+
 driver.find_element(By.ID, "a").send_keys("6")
 driver.find_element(By.ID, "b").send_keys("5")
+
 driver.find_element(By.ID, "gomb1").click()
 
+header_text = driver.find_element(By.XPATH, "//h1").text
+assert header_text == "Calc"
+
+driver.close()
 
