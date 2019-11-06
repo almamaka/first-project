8d8afca01860bdae17134a3fc2585462edf46aae
almamaka <esztzi@gmail.com>
2019-11-06 19:53:22 +0100

    print(header_text) hozzáadása

--- szamolj.py
+++ szamolj.py
@@ -5,10 +5,10 @@ driver = webdriver.Chrome()
 
 driver.get("https://almamaka.github.io/first-project/")
 
-driver.find_element(By.ID, "a").send_keys("6")
-driver.find_element(By.ID, "b").send_keys("5")
+driver.find_element(By.NAME, "a").send_keys("6")
+driver.find_element(By.NAME, "b").send_keys("5")
 
-driver.find_element(By.ID, "gomb1").click()
+driver.find_element(By.NAME, "gomb1").click()
 
 header_text = driver.find_element(By.XPATH, "//h1").text
 print(header_text)
