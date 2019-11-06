d3bd7648a398d0a1c572c5232d559b975b272ade
almamaka <esztzi@gmail.com>
2019-11-06 20:51:10 +0100

    bekért számolás added

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
 print("Hello Python!")
+
+first_number = input("Adj meg egy számot: ")
+second_number = input("Mivel adjam össze? ")
+print(int(first_number) + int(second_number))
--- szamolj.py
+++ szamolj.py
@@ -6,11 +6,16 @@ driver = webdriver.Chrome()
 driver.get("https://almamaka.github.io/first-project/")
 #driver.find_element(By.NAME, "a").send_keys("6")
 
-first_input_field = driver.find_element(By.NAME, "a")
-first_input_field.send_keys("6")
-first_input_field.screenshot("first-input.png")
+first_number = input("Adj meg egy számot! ")
+second_number = input("Mivel adjam össze? ")
 
-driver.find_element(By.NAME, "b").send_keys("5")
+first_input_field = driver.find_element(By.NAME, "a")
+first_input_field.send_keys(first_number)
+#first_input_field.screenshot("first-input.png")
+
+#driver.find_element(By.NAME, "b").send_keys("5")
+second_input_field = driver.find_element(By.NAME, "b")
+second_input_field.send_keys(second_number)
 
 driver.find_element(By.NAME, "gomb1").click()
 result = driver.find_element_by_id("result-input").get_attribute("value")
@@ -18,7 +23,14 @@ print(result)
 
 driver.save_screenshot("result.png")
 
-assert result == "11"
+expected = int(first_number) + int(second_number)
+print(expected)
+print(result)
+
+print(type(expected))
+print(type(result))
+
+assert int(result) == expected
 
 header_text = driver.find_element(By.XPATH, "//h1").text
 print(header_text)
