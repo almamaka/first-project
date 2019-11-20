f20c4a772d4c72dc728dc92423df5605146f17db
almamaka <esztzi@gmail.com>
2019-11-20 18:23:32 +0100

    Employee hibaüzenetes added

---
+++ create_employee.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver = webdriver.Chrome()
+
+driver.get("http://www.learnwebservices.com/empapp/create-employee.xhtml")
+driver.find_element(By.ID, 'create-form:name-input').click()
+
+WebDriverWait(driver, 10).until(
+    expected_conditions.text_to_be_present_in_element((By.XPATH, "//span[@class = 'invalid-feedback']"),
+                                                      'Az alkalmazott nevét meg kell adni!')
+)
+
+driver.find_element(By.XPATH, '//html/body/div/h1').click()
+hibauzenet = driver.find_element(By.XPATH, "//span[@class = 'invalid-feedback']").text
+assert hibauzenet == 'Az alkalmazott nevét meg kell adni!'
+#//*[@id="create-form:name-form-group"]/span
+print(hibauzenet)
