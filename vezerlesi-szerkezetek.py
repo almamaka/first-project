ee25e7ce554cc0059f129d648b05d749b2b000e3
almamaka <esztzi@gmail.com>
2019-11-27 18:28:32 +0100

    vezerlesi szerkezetek added

---
+++ vezerlesi-szerkezetek.py
@@ -0,0 +1,10 @@
+age = 60
+age_is_greater_than_50 = age > 50;
+print (age_is_greater_than_50)
+if age_is_greater_than_50:
+    print("Eletkor nagyobb mint 50")
+    print("Sajnos")
+
+else:
+    print("Eletkor kisebb mint 50")
+print("Program vege")
