e1a5962fde821608f87312510e350b8f022b9abd
almamaka <esztzi@gmail.com>
2019-12-18 20:08:00 +0100

    SokSelf pytest added

--- pages/details_page.py
+++ pages/details_page.py
@@ -8,3 +8,13 @@ class LocationsDetailsPage:
 
     def click_to_back_to_list_link(self):
         self.driver.find_element_by_link_text("Back to list").click()
+
+    def assert_message_has_been_appeared(self, expected_message):
+        message = self.driver.find_element_by_class_name("alert").text
+        assert message == expected_message
+
+    def assert_details_are(self, expected_name, expected_coords):
+        name = self.driver.find_element_by_xpath("//tr[td[1][text() = 'Name']]/td[2]").text
+        coords = self.driver.find_element_by_xpath("//tr[td[1][text() = 'Coordinates']]/td[2]").text
+        assert name == expected_name
+        assert coords == expected_coords
--- tests/test_locations.py
+++ tests/test_locations.py
@@ -1,17 +1,35 @@
 from selenium import webdriver
 
 from db.db_operations import DbOperation
+from pages.create_location_page import CreateLocationPage
+from pages.details_page import LocationsDetailsPage
 from pages.list_locations_page import ListLocationsPage
 
 
 class TestLocations:
 
-    def test_empty_table(self):
-        driver = webdriver.Chrome()
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
         db_operations = DbOperation()
         db_operations.delete_locations()
+        self.list_page = ListLocationsPage(self.driver)
+        self.create_location = CreateLocationPage(self.driver)
+        self.details_page = LocationsDetailsPage(self.driver)
 
-        list_page = ListLocationsPage(driver)
-        list_page.go()
-        list_page.assert_title_is_ok()
-        list_page.assert_count_of_table_rows_is(0)
+    def teardown_method(self):
+        self.driver.close()
+
+    def test_empty_table(self):
+        self.list_page.go()
+        self.list_page.assert_title_is_ok()
+        self.list_page.assert_count_of_table_rows_is(0)
+
+    def test_when_create_location_then_details_are_correct(self):
+        self.list_page.go()
+        self.list_page.click_to_create_location_link()
+
+        self.create_location.fill_create_location_form("Budapest", "1,1")
+        self.create_location.click_to_create_button()
+
+        self.details_page.assert_details_are("Budapest", "1.0, 1.0")
+        self.details_page.assert_message_has_been_appeared("Location has been created.")
